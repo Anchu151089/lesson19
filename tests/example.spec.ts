bb62f38f74f1a75c55d8dab6e497ae8652d353b7
anchu <anchualavungal@gmail.com>
2025-08-07 10:41:54 +0300

    initial commit of homework 19

---
+++ tests/example.spec.ts
@@ -0,0 +1,101 @@
+import { test, expect } from '@playwright/test';
+import {ToDoItem} from "./toDoPage";
+
+test.beforeEach(async ({ page }) => {
+
+  const todo = new ToDoItem(page);
+  await todo.goto();
+
+});
+
+test('has title', async ({ page }) => {
+
+  const todo = new ToDoItem(page);
+  await todo.addItems('item 1')
+  await todo.visible()
+  await page.pause()
+});
+test('create two items ', async ({ page }) => {
+
+  const todo = new ToDoItem(page);
+  await todo.addItems('item 1')
+  await todo.addItems('item 2')
+
+  const toDoItemNo =await  todo.visible()
+  console.log('number of items added = '+ toDoItemNo);
+  expect(toDoItemNo).toBe(2)
+  await page.pause()
+});
+
+test('create an item and select that item', async ({ page }) => {
+
+  const todo = new ToDoItem(page);
+  await todo.addItems('item 1')
+  await todo.selectItem();
+  await todo.expectTodoCompleted()
+  await page.pause()
+});
+
+test('create an item and select that item and delete that item', async ({ page }) => {
+
+  const todo = new ToDoItem(page);
+  await todo.addItems('item 1')
+  await todo.selectItem();
+  await todo.hover()
+  await todo.button()
+  await page.pause()
+});
+
+test('create two item and select one item and click clear completed', async ({ page }) => {
+
+  const todo = new ToDoItem(page);
+  await todo.addItems('item 1')
+  await todo.selectItem();
+  await todo.addItems('item 3')
+  const toDoItemNo =  await todo.getCount()
+  expect(toDoItemNo).toBe(2)
+  await todo.completeButton()
+  const toDoItemNoAfterDelete = await todo.getCount()
+  expect(toDoItemNoAfterDelete).toBe(1)
+  await page.pause()
+
+});
+
+test('create two item and click completed', async ({ page }) => {
+  const todo = new ToDoItem(page);
+  await todo.addItems('item 1')
+  await todo.selectItem();
+  await todo.addItems('item 34')
+  await todo.completedLinkCheck()
+  const toDoItemNo =await todo.getCount()
+  expect(toDoItemNo).toBe(1)
+  console.log(toDoItemNo)
+  await page.pause()
+});
+test('create three item and click active', async ({ page }) => {
+
+  const todo = new ToDoItem(page);
+  await todo.addItems('item 1')
+  await todo.selectItem();
+  await todo.addItems('item 11')
+  await todo.addItems('item 12')
+  await todo.activeLinkCheck()
+  const toDoItemNo =await todo.getCount()
+  expect(toDoItemNo).toBe(2)
+  console.log(toDoItemNo)
+  await page.pause()
+});
+test.only('create three item select one and click All button', async ({ page }) => {
+
+  const todo = new ToDoItem(page);
+  await todo.addItems('item 1')
+  await todo.selectItem();
+  await todo.addItems('item 11')
+  await todo.addItems('item 12')
+  await todo.allButtonChecked()
+  const toDoItemNo =await todo.getCount()
+  expect(toDoItemNo).toBe(3)
+  console.log(toDoItemNo)
+  await page.pause()
+});
+
---
+++ tests/toDoPage.ts
@@ -0,0 +1,83 @@
+import {expect, Locator, Page} from "@playwright/test";
+
+
+export class ToDoItem
+{
+    readonly page : Page
+    readonly textInput : Locator;
+    readonly toDoItemLabel : Locator;
+    readonly todoItemToggle: Locator;
+    readonly todoItem : Locator;
+    readonly toDoItemButton : Locator;
+    readonly clearCompleteButton : Locator;
+    readonly completeLink : Locator;
+    readonly activeLink : Locator;
+    readonly selectAllLinks : Locator;
+
+    constructor(page : Page)
+    {
+        this.page = page;
+        this.textInput = page.getByTestId('text-input');
+        this.toDoItemLabel = page.getByTestId('todo-item-label');
+        this.todoItemToggle = page.getByTestId('todo-item-toggle');
+        this.todoItem = page.getByTestId('todo-item');
+        this.toDoItemButton = page.getByTestId('todo-item-button');
+        this.clearCompleteButton = page.getByText('Clear completed');
+        this.completeLink = page.getByRole('link', { name: 'Completed' });
+        this.activeLink = page.getByRole('link', { name: 'Active' });
+        this.selectAllLinks = page.getByRole('link', { name: 'All' });
+
+    }
+
+    async goto()
+    {
+        await this.page.goto('https://todo-app.tallinn-learning.ee/');
+    }
+    async addItems(text: string)
+    {
+        await this.textInput.fill(text);
+        await this.textInput.press('Enter');
+
+    }
+    async visible()
+    {
+        await  expect(this.toDoItemLabel).toBeVisible();
+    }
+    async selectItem()
+    {
+        await this.todoItemToggle.click()
+    }
+    async expectTodoCompleted()
+    {
+        await expect(this.todoItem).toHaveClass(/completed/);
+    }
+    async hover()
+    {
+       await this.toDoItemLabel.hover()
+    }
+    async button()
+    {
+        await this.toDoItemButton.click()
+    }
+    async getCount()
+    {
+        return await this.toDoItemLabel.count()
+    }
+    async completeButton()
+    {
+        await this.clearCompleteButton.click()
+    }
+    async completedLinkCheck()
+    {
+        await this.completeLink.click()
+    }
+    async activeLinkCheck()
+    {
+        await this.activeLink.click()
+    }
+    async allButtonChecked()
+    {
+        await this.selectAllLinks.click()
+    }
+
+}
